41c4bbb742f9ac3749043079adbc6b95fc87d6d2
luizcapella <luizcapella06@gmail.com>
2026-09-16 09:36:47 -0300

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://wmizfiabviqcclfaivza.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtaXpmaWFidmlxY2NsZmFpdnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNjAwODIsImV4cCI6MjEwNDgzNjA4Mn0.gW52vsp_23-hgX7vGxQDueuPyiLR1hXL45fsk2hZ7WI';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
