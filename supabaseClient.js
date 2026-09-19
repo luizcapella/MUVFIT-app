d4d84476664acacc5c7815bbea1147d86c3a564c
luizcapella <luizcapella06@gmail.com>
2026-09-19 16:54:50 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,13 +1,22 @@
 import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 
 const supabaseUrl = 'https://wmizfiabviqcclfaivza.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtaXpmaWFidmlxY2NsZmFpdnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3MTAyNjMsImV4cCI6MjA0MjI4NjI2M30.4s8_J9P26J33f0_0yI92u6d3p2u9q31_y3q8p0p7w';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtaXpmaWFidmlxY2NsZmFpdnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3MTAyNjMsImV4cCI6MjA0MjI4NjI2M30.4s8_J9P26J33f0_0y4s8_J9P26J33f0_0y4s8_J9P26J';
+
+let storageOption;
+
+if (Platform.OS !== 'web') {
+  try {
+    storageOption = require('@react-native-async-storage/async-storage').default;
+  } catch (e) {
+    storageOption = undefined;
+  }
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: Platform.OS === 'web' ? undefined : AsyncStorage,
+    storage: storageOption,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
