fde0f1372065d51914c8ddb431917ead1a4ead92
luizcapella <luizcapella06@gmail.com>
2026-09-21 11:25:28 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wmizfiabviqcclfaivza.supabase.co';
-const supabaseAnonKey = 'SUA_CHAVE_COMPLETA_E_REAL_AQUI';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtaXpmaWFidmlxY2NsZmFpdnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNjAwODIsImV4cCI6MjEwNDgzNjA4Mn0.gW52vsp_23-hgX7vGxQDueuPyiLR1hXL45fsk2hZ7WI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
